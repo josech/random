ed9e09c09151a60724c4d44a3597211c36ab35e5
Jose Martinez <xjosechx@gmail.com>
2015-07-05 19:12:52 -0500

    random version 2

--- random.js
+++ random.js
@@ -4,7 +4,7 @@ var numero = Math.random();
 var str =' MAYOR que 0,5';
 
 if (numero <= 0.5){
-  str = ' MENOR que 0,5';
+  str = ' MENOR o IGUAL que 0,5';
 }
 
 console.log('\n' + numero + str + '\n');
